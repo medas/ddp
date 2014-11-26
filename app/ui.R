3e9e60b3180d0264de0aa5ea3304a0ac7c13072e
medas <pedro.medas@gmail.com>
2014-11-26 00:12:50 +0000

    all

---
+++ app/ui.R
@@ -0,0 +1,48 @@
+shinyUI(fluidPage(
+    titlePanel('Graphical manipulation'),
+    fluidRow(
+        column(4, wellPanel(
+            selectInput('xcol', 'X Variable', names(iris)),
+            selectInput('ycol', 'Y Variable', names(iris),
+                        selected=names(iris)[[2]]),
+            helpText('This is an exercise to see some of the shiny features.',
+                     'It used the iris dataset to show the result of a k-mean
+                     analysis.',
+                     'First, pick the variable to use on the k-means anlysis.'),
+            helpText(
+                     'It is also possible to choose the number of clusters to 
+                     used on the analysis.'
+                     ),
+            helpText('You can also costumize several aspects of the graphic:',
+                     'the shape and size of the dot representing the data',
+                     'and also the title and labels of the axis.')
+            )),
+        column(8, wellPanel(
+            plotOutput('plot1')
+        ))
+        ),
+    fluidRow(
+        column(3, wellPanel(
+            h5("graphical details"),
+            sliderInput('clusters',
+                        h6("Number of Clusters"),
+                        min = 1, max = 9, value = 3, step = 1,
+                        animate = animationOptions(interval = 300, loop = TRUE)
+                        ),
+            sliderInput("pchs",
+                        h6("Choose the point type to use on the graph"),
+                        min = 0, max = 25, value = 20, step = 1,
+                        animate = animationOptions(interval = 600, loop = TRUE)
+                        ),
+            sliderInput("cexs", h6("Now choose the point display size"),
+                            min = 1, max = 24, value = 5, step = 1,
+                            animate = animationOptions(interval = 1000, loop = TRUE))
+            )),
+        column(3, wellPanel(
+            h5("graph costumization"),
+            textInput("title", label = h6("plot title"), value = "Iris k-means clustering"),
+            textInput("xlab", label = h6("horizontal label"), value = names(iris)[[1]]),
+            textInput("ylab", label = h6("vertical label"), value = names(iris)[[2]])
+            ))
+        )
+))
---
+++ presenting.md
@@ -0,0 +1,45 @@
+presenting a shiny app
+========================================================
+author: 
+date: November 2014
+
+First Slide
+========================================================
+
+This presentation describes how my shiny application works.
+
+The app is available on https://www.shinyapps.io/
+a repository for applications done with shiny. :-)
+
+The application can be run from here: https://coding.shinyapps.io/ddpshinyapp/
+
+Brief description
+========================================================
+
+The application uses the iris data set available for R and is an applicaiton of k-mean to determine the clusters on a data set.
+To learn more about k-mean check the R documentation (or seach the internet) :).
+
+The application shows how to manipulate several aspects of the plot and also
+allows fot you to change the variables used and the number of kernels used to infer 
+clusters from the data.
+
+Slide With Plot
+========================================================
+
+The kmeans will try to find the numbers seletec on the data. A plot similar to the one here will beseen if you do a analysis with 4 cluester, the colors are dipecting the clusters and the kernels are displayed as asterics (*).
+
+```r
+clusters <- kmeans(iris[, 3, 1], 4)
+plot(iris[, 3, 1], col = clusters$cluster, pch = 20, cex = 3)
+points(clusters$centers, pch = 8, cex = 5, lwd = 3)
+```
+
+![plot of chunk unnamed-chunk-1](presenting-figure/unnamed-chunk-1-1.png) 
+
+The application
+========================================================
+On the application we can change the variables we are trying to model, choose from the dropbox the x and y variables. 
+We can also  put the sliders on an animation mode and see the plot change automatically.
+We can also personalize the axis labels and the title.
+
+On the right side we can see the result of changing the clusters being predicted on the plot of the model.
